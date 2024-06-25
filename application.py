425aacd54fc19b0fe44937c5fef116968364e757
Manuel Cobos <flipper7247@gmail.com>
2024-06-25 02:11:24 -0500

    Add service for pokemon table

--- application.py
+++ application.py
@@ -3,9 +3,11 @@
 from src.settings import application
 from src.scripts.routes import base_blueprint
 from src.scripts.routes import servicio_t_character
+from src.scripts.routes import servicio_t_pokemon
 
 application.register_blueprint(base_blueprint)
 application.register_blueprint(servicio_t_character)
+application.register_blueprint(servicio_t_pokemon)
 
 if __name__ == '__main__':
     application.run()
